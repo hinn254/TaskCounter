18d6144a5a123dd960013124e79c11c4b8155289
Benny <bennyhinnotieno@gmail.com>
2020-08-29 17:55:55 +0300

    Updates

---
+++ precache-manifest.454478a1961b3ea6745fdc493563bb5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a4f956b2ed3c1266a76ad12426027bf",
+    "url": "/TaskCounter/index.html"
+  },
+  {
+    "revision": "2c86a93d0871ae60a9ed",
+    "url": "/TaskCounter/static/css/main.5377a181.chunk.css"
+  },
+  {
+    "revision": "04e55a4aa9ab2a331843",
+    "url": "/TaskCounter/static/js/2.2dff032a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TaskCounter/static/js/2.2dff032a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c86a93d0871ae60a9ed",
+    "url": "/TaskCounter/static/js/main.cb887b26.chunk.js"
+  },
+  {
+    "revision": "652686b95d2a63bc6b16",
+    "url": "/TaskCounter/static/js/runtime-main.02b5fa73.js"
+  }
+]);
